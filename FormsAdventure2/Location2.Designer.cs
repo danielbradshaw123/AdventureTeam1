8faee42c9eaa19a41019a4fb69803beb965f9d83
Alec Hampson <alec.hampson@student.portside.sa.edu.au>
2019-05-12 19:28:11 +0930

    Explained more

--- FormsAdventure2/Location2.Designer.cs
+++ FormsAdventure2/Location2.Designer.cs
@@ -86,9 +86,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 6);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(206, 17);
+            this.label1.Size = new System.Drawing.Size(226, 34);
             this.label1.TabIndex = 8;
-            this.label1.Text = "Detention Room - Second Slide";
+            this.label1.Text = "Detention Room - Second Slide\r\nMr Branton Guaranteed Response";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Location2
