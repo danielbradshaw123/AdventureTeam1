56fd358c8f61c49fa484b174d0e492b703e34d2b
Will No <wilmar@internode.on.net>
2019-05-29 09:37:30 +0930

    Forgot something

--- FormsAdventure2/Location10.Designer.cs
+++ FormsAdventure2/Location10.Designer.cs
@@ -60,9 +60,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(14, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(209, 20);
+            this.label1.Size = new System.Drawing.Size(135, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Detention Room - First Slide";
+            this.label1.Text = "Gym - Tenth Slide";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button3
