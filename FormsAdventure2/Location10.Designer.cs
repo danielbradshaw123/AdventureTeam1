1af8b1dad58c08a920a6960065063541ec0d686c
Will No <wilmar@internode.on.net>
2019-05-29 09:31:26 +0930

    Location 10 Done

--- FormsAdventure2/Location10.Designer.cs
+++ FormsAdventure2/Location10.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Location10));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
@@ -44,21 +43,24 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Malgun Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.textBox1.Location = new System.Drawing.Point(1, 489);
+            this.textBox1.Location = new System.Drawing.Point(1, 630);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(1482, 215);
+            this.textBox1.Size = new System.Drawing.Size(1667, 249);
             this.textBox1.TabIndex = 2;
-            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.Text = "Once inside of the gym, Sam meets up with Zack, who is distracted by a life-size " +
+    "grumpy cat\r\nSam:\r\n“Zack c’mon man we gotta go find the others!”\r\nZack:\r\n“Yeah ju" +
+    "st give me a couple of minutes...”\r\n\r\n";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(14, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(185, 17);
+            this.label1.Size = new System.Drawing.Size(209, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Detention Room - First Slide";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -68,10 +70,10 @@
             this.button3.BackColor = System.Drawing.Color.Navy;
             this.button3.Font = new System.Drawing.Font("Malgun Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(0, 704);
+            this.button3.Location = new System.Drawing.Point(0, 880);
             this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(497, 153);
+            this.button3.Size = new System.Drawing.Size(559, 191);
             this.button3.TabIndex = 4;
             this.button3.Text = "“Thanks. \r\nHave a good night Mr Branton.”";
             this.button3.UseVisualStyleBackColor = false;
@@ -82,10 +84,10 @@
             this.button1.BackColor = System.Drawing.Color.Navy;
             this.button1.Font = new System.Drawing.Font("Malgun Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(495, 704);
+            this.button1.Location = new System.Drawing.Point(557, 880);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(494, 153);
+            this.button1.Size = new System.Drawing.Size(556, 191);
             this.button1.TabIndex = 5;
             this.button1.Text = "Say nothing and just leave.";
             this.button1.UseVisualStyleBackColor = false;
@@ -95,10 +97,10 @@
             this.button2.BackColor = System.Drawing.Color.Navy;
             this.button2.Font = new System.Drawing.Font("Malgun Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(986, 704);
+            this.button2.Location = new System.Drawing.Point(1109, 880);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(497, 153);
+            this.button2.Size = new System.Drawing.Size(559, 191);
             this.button2.TabIndex = 6;
             this.button2.Text = "“Oh Mr Branton, can’t wait for the next funny video of the week!” 😂😂😂😂😂😂😂😂" +
     "😂”";
@@ -116,17 +118,18 @@
             "Text",
             "Button options",
             "Characters"});
-            this.checkedListBox1.Location = new System.Drawing.Point(1082, 139);
+            this.checkedListBox1.Location = new System.Drawing.Point(1217, 174);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(211, 106);
+            this.checkedListBox1.Size = new System.Drawing.Size(237, 130);
             this.checkedListBox1.TabIndex = 7;
             // 
-            // Location1Template1
+            // Location10
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(1482, 860);
+            this.ClientSize = new System.Drawing.Size(1667, 1050);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -134,7 +137,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Name = "Location1Template1";
+            this.Name = "Location10";
             this.Text = "      ";
             this.Load += new System.EventHandler(this.Location1_Load);
             this.ResumeLayout(false);
