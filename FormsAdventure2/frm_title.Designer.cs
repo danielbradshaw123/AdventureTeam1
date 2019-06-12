c49c88b0b8633b876f042c6f2015d29a638d3082
Sam eissenberg <sammyg2233@gmail.com>
2019-06-12 09:51:27 +0930

    Title screen change

--- FormsAdventure2/frm_title.Designer.cs
+++ FormsAdventure2/frm_title.Designer.cs
@@ -53,12 +53,12 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(803, 476);
+            this.button1.Location = new System.Drawing.Point(788, 476);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(229, 68);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Start";
+            this.button1.Text = "To start, close this window.";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
